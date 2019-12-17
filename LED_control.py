e0b39ff906be3b5f82c4d9f78ad51af0021f4472
htgdokania <dokaniaharsh@gmail.com>
2019-12-17 20:42:46 +0000

    conf

--- LED_control.py
+++ LED_control.py
@@ -2,5 +2,5 @@ from boltiot import Bolt
 api_key = "b19b11ae-3124-4686-ac2b-8d554b6d7870"
 device_id  = "BOLT294229"
 mybolt = Bolt(api_key, device_id)
-response = mybolt.digitalWrite('0', 'HIGH')
+response = mybolt.digitalWrite('0', 'LOW')
 print (response)
--- led_brightness.py
+++ led_brightness.py
@@ -2,5 +2,8 @@ from boltiot import Bolt
 api_key = "b19b11ae-3124-4686-ac2b-8d554b6d7870"
 device_id  = "BOLT294229"
 mybolt = Bolt(api_key, device_id)
-response = mybolt.analogWrite('0', '100')
-print (response)
+i='0'
+for i in range(255):
+	response = mybolt.analogWrite('0', i)
+	print (response,i)
+
