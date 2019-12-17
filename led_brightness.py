4c37b0919d1ac92dc63ee87593b79764913f2875
HARSH DOKANIA <34301506+htgdokania@users.noreply.github.com>
2019-12-18 00:42:09 +0530

    Create led_brightness.py

---
+++ led_brightness.py
@@ -0,0 +1,6 @@
+from boltiot import Bolt
+api_key = "b19b11ae-3124-4686-ac2b-8d554b6d7870"
+device_id  = "BOLT294229"
+mybolt = Bolt(api_key, device_id)
+response = mybolt.analogWrite('0', '100')
+print (response)
