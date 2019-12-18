fbe13b0785528ae68baa86a1b55e99a1c49bdb65
htgdokania <dokaniaharsh@gmail.com>
2019-12-18 12:51:02 +0000

    removed led_brigtness code

--- led_brightness.py
+++
@@ -1,9 +0,0 @@
-from boltiot import Bolt
-api_key = "b19b11ae-3124-4686-ac2b-8d554b6d7870"
-device_id  = "BOLT294229"
-mybolt = Bolt(api_key, device_id)
-i='0'
-for i in range(255):
-	response = mybolt.analogWrite('0', i)
-	print (response,i)
-
